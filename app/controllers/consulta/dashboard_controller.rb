1867589f90648ea821c71f9f8bd19d5a74c54219
Oscar Nuñez <oscarnunez@elquick.(none)>
2014-10-03 04:37:45 -0600

    Maquetacion Front

--- app/controllers/consulta/dashboard_controller.rb
+++ app/controllers/consulta/dashboard_controller.rb
@@ -2,7 +2,6 @@ class Consulta::DashboardController < ConsultaController
   layout 'consulta'
 
   def index
-
     @events = CnsEvent.limit(3).decorate rescue nil
     @articles = CnsArticle.limit(3).decorate rescue nil
   end
