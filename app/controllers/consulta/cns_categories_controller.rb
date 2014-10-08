60a12c88756fc6ac1985277c6ef7984975d93908
Oscar Nuñez <oscarnunez@elquick.(none)>
2014-10-08 12:12:14 -0600

    front para categorias y propuestas

--- app/controllers/consulta/cns_categories_controller.rb
+++ app/controllers/consulta/cns_categories_controller.rb
@@ -3,11 +3,19 @@ class Consulta::CnsCategoriesController < ApplicationController
 
   def index 
     @categories = CnsCategory.all
+    show_breadcrumbs
   end
 
   def show
     @category = CnsCategory.find(params[:id])
     @proposals = CnsProposal.where("cns_category_id = ?", @category.id)
+    show_breadcrumbs(@category)
   end
 
+  private
+    def show_breadcrumbs category=nil
+      add_breadcrumb I18n.t('layouts.consulta.home'), root_path
+      add_breadcrumb I18n.t('layouts.consulta.categories'), consulta_cns_categories_path
+      add_breadcrumb @category.name, consulta_cns_category_path(category) unless category.nil?
+    end
 end
--- app/controllers/consulta/cns_proposals_controller.rb
+++ app/controllers/consulta/cns_proposals_controller.rb
@@ -2,11 +2,25 @@ class Consulta::CnsProposalsController < ApplicationController
   layout 'consulta'
 
   def index 
+    @category = CnsCategory.find(params[:cns_category_id])
     @proposals = CnsProposal.all
+    show_breadcrumbs(@category)
   end
 
   def show
+    @category = CnsCategory.find(params[:cns_category_id])
     @proposal = CnsProposal.find(params[:id])
+    show_breadcrumbs(@category, @proposal)
   end
+
+  private
+    def show_breadcrumbs(category=nil, proposal=nil)
+      add_breadcrumb I18n.t('layouts.consulta.home'), root_path
+      add_breadcrumb I18n.t('layouts.consulta.categories'), consulta_cns_categories_path
+      add_breadcrumb @category.name, consulta_cns_category_path(category) unless category.nil?
+      add_breadcrumb I18n.t('layouts.consulta.proposals'), consulta_cns_category_cns_proposals_path(category) unless category.nil?
+      add_breadcrumb @proposal.name, consulta_cns_category_cns_proposal_path(category, proposal) unless category.nil? or proposal.nil?
+
+    end
 
 end
