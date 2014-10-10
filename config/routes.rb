b910c16a362d1a52ad8600d71b9acdabefb0e59c
Henry Díaz <henry@henry-HP-ProBook-6475b.(none)>
2014-10-10 11:57:38 -0600

    Cambios consulta

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   end
 
 
-  constraints subdomain: 'admin' do
+  constraints subdomain: 'apps.gobiernoabierto' do
     scope module: 'admin', as: 'admin' do
       defaults subdomain: 'admin' do
 
