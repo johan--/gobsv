50ead93dc65c0238dceedc167050d0c6321cb82b
Henry Díaz <henry@henry-HP-ProBook-6475b.(none)>
2014-10-14 10:32:14 -0600

    Segundo commit para los comentarios

--- app/controllers/consulta/cns_comments_controller.rb
+++ app/controllers/consulta/cns_comments_controller.rb
@@ -2,6 +2,19 @@ class Consulta::CnsCommentsController < ConsultaController
   respond_to :js
 
   def create
-
+    @comment = CnsComment.new item_params
+    @comment.cns_proposal_id = proposal.try(:id)
+    @comment.save
   end
+
+
+  def item_params
+    params.require(:cns_comment).permit(:name, :email, :content)
+  end
+
+  private
+    def proposal
+      @proposal ||= CnsProposal.find(params[:cns_proposal_id])
+    end
+    helper_method :proposal
 end
