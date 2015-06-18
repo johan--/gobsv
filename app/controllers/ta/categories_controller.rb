84c8a53a6db528b41bb7cf3846452e89317ed6a7
el_quick <el.quick@gmail.com>
2015-06-18 11:52:22 -0600

    metas

--- app/controllers/ta/categories_controller.rb
+++ app/controllers/ta/categories_controller.rb
@@ -10,6 +10,21 @@ class Ta::CategoriesController < TaController
     @yesterday_articles = Ta::Article.yesterday.publish.newer.limit(3)
 
     @featured = Ta::Article.featured.newer.limit(5)
+
+    set_meta_tags ({
+      title: 'Noticias sobre transparencia, acceso a la Información y anticorrupción en El Salvador',
+      site: 'TRANSPARENCIA ACTIVA',
+      description: 'Transparencia Activa es un periódico digital que divulga las acciones de transparencia desarrolladas, rendición de cuentas, acceso a la información y lucha',
+      keywords: 'noticias el salvador, trasparencia el salvador, transparencia activa, gobierno de el salvador, informacion el salvador, presidente funes,  informacion publica el salvador, noticias de verdad, gobierno del cambio',
+      canonical: ta_root_url,
+      author: 'Transparencia Activa',
+      og: {
+        title: 'Noticias sobre transparencia, acceso a la Información y anticorrupción en El Salvador',
+        image: URI.join(request.url, 'assets/ta/missing.jpg'),
+        description: 'Transparencia Activa es un periódico digital que divulga las acciones de transparencia desarrolladas, rendición de cuentas, acceso a la información y lucha',
+        url: ta_root_url
+      }
+    })
   end
 
   def show
