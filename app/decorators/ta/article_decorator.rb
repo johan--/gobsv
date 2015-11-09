2d847588b1862412c8ea0f252ef0c5d6048d6935
el_quick <el.quick@gmail.com>
2015-11-09 08:08:42 -0600

    formato fecha notas TA

--- app/decorators/ta/article_decorator.rb
+++ app/decorators/ta/article_decorator.rb
@@ -2,6 +2,6 @@ class Ta::ArticleDecorator < Draper::Decorator
   delegate_all
 
   def created_at
-    h.l object.created_at, format: :default
+    h.l object.created_at, format: :sortable
   end
 end
