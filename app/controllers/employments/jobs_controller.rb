699c5bd6ead777c5746d59b7f8980b3679e577a5
Henry Díaz <henryalberto.diaz@gmail.com>
2015-12-09 08:30:34 -0600

    Cambios en empleos

--- app/controllers/employments/jobs_controller.rb
+++ app/controllers/employments/jobs_controller.rb
@@ -2,7 +2,7 @@ class Employments::JobsController < EmploymentsController
   respond_to :html, :js
 
   def index
-    @jobs = Employments::PublicCompetition.valid.ransack(params[:q]).result(distinct: true).paginate(page: params[:page])
+    @jobs = Employments::PublicCompetition.valid.ransack(params[:q]).result(distinct: true).paginate(page: params[:page], per_page: 5)
     add_breadcrumb 'Inicio', employments_root_url
     add_breadcrumb "Empleos disponibles", nil
   end
--- app/models/employments/public_competition.rb
+++ app/models/employments/public_competition.rb
@@ -1,3 +1,7 @@
 class Employments::PublicCompetition < ActiveRecord::Base
   scope :valid, -> { where.not(plaza_state_id: nil) }
+
+  has_many :technical_competences, foreign_key: :plaza_id, primary_key: :plaza_id
+  has_many :experiences, foreign_key: :plaza_id, primary_key: :plaza_id
+  has_many :degrees, foreign_key: :plaza_id, primary_key: :plaza_id
 end
