14eee001abc060d90fbd35a66ff411ecf090a90b
Henry Díaz <henryalberto.diaz@gmail.com>
2015-12-14 14:52:43 -0600

    Plazas disponibles

--- app/models/employments/public_competition.rb
+++ app/models/employments/public_competition.rb
@@ -1,5 +1,5 @@
 class Employments::PublicCompetition < ActiveRecord::Base
-  scope :valid, -> { where.not(plaza_state_id: nil) }
+  scope :valid, -> { where(plaza_state_id: 2) }
   scope :active, -> { where(active: true) }
 
   has_many :technical_competences, foreign_key: :plaza_id, primary_key: :plaza_id
