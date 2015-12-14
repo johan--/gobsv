3ab64a23837e0f58174ece2a3593cdfad8a93ec4
Henry Díaz <henryalberto.diaz@gmail.com>
2015-12-14 11:27:49 -0600

    Cambios en empleos

--- app/models/employments/factor.rb
+++ app/models/employments/factor.rb
@@ -1,2 +1,4 @@
 class Employments::Factor < ActiveRecord::Base
+  default_scope { order(order: :asc) }
+  has_many :areas, foreign_key: :factor_score_id, primary_key: :factor_score_id
 end
--- app/models/employments/public_competition.rb
+++ app/models/employments/public_competition.rb
@@ -5,4 +5,5 @@ class Employments::PublicCompetition < ActiveRecord::Base
   has_many :technical_competences, foreign_key: :plaza_id, primary_key: :plaza_id
   has_many :experiences, foreign_key: :plaza_id, primary_key: :plaza_id
   has_many :degrees, foreign_key: :plaza_id, primary_key: :plaza_id
+  has_many :factors, foreign_key: :plaza_id, primary_key: :plaza_id
 end
