10ee8b7bf31065f7dca9096320bae3f7b58b1595
el_quick <el.quick@gmail.com>
2016-01-12 10:36:10 -0600

    Comentarios

---
+++ app/controllers/admin/ta/comments_controller.rb
@@ -0,0 +1,32 @@
+class Admin::Ta::CommentsController < Admin::TaController
+
+  def model
+    ::Ta::Comment
+  end
+
+  def table_columns
+    %w(article_title name email status created_at)
+  end
+
+  def index
+    @items = model
+      .joins(:article)
+      .select(:id, :name, :email, :status, :created_at)
+      .select('(SUBSTRING("ta_articles"."title", 0, 45) || \'...\') AS article_title')
+      .where(conditions).decorate
+
+    add_breadcrumb model.model_name.human(count: :many), index_url
+
+    respond_to do |format|
+      format.html { render template: 'concerns/tabled/index' }
+      format.json { render json: @items }
+    end
+  end
+
+  def item_params
+    params.require(:ta_comment).permit(
+      :message,
+      :status
+    )
+  end
+end
