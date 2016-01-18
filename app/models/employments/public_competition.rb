7f4461153376769fa7dd92abefd4aff576c2f16a
Henry Díaz <henryalberto.diaz@gmail.com>
2016-01-18 08:59:16 -0600

    Fecha en el filtro de plazas

--- app/models/employments/public_competition.rb
+++ app/models/employments/public_competition.rb
@@ -1,6 +1,6 @@
 class Employments::PublicCompetition < ActiveRecord::Base
   scope :valid, -> { where(plaza_state_id: 2) }
-  scope :active, -> { where(active: true).where("? BETWEEN employments_public_competitions.opening_registration AND employments_public_competitions.closing_registration", Time.current) }
+  scope :active, -> { where(active: true).where("? BETWEEN employments_public_competitions.opening_registration AND employments_public_competitions.closing_registration", Date.current) }
 
   has_many :technical_competences, foreign_key: :plaza_id, primary_key: :plaza_id
   has_many :experiences, foreign_key: :plaza_id, primary_key: :plaza_id
