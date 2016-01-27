5163e1dd7e384206386b638e80c3712164a8f76c
Henry Díaz <henryalberto.diaz@gmail.com>
2016-01-27 08:00:33 -0600

    Cambios en la consulta de las plazas publicadas

--- app/controllers/employments/dashboard_controller.rb
+++ app/controllers/employments/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Employments::DashboardController < EmploymentsController
 
   def index
-    @publish_plazas = Employments::PublicCompetition.active.where.not(plaza_state_id: 1)
+    @publish_plazas = Employments::PublicCompetition.where(active: true).where.not(plaza_state_id: 1)
     @total_plazas = Employments::PublicCompetition.active.valid.count.to_s.rjust(4, '0')
     @last_plazas = Employments::PublicCompetition.active.valid.order(updated_date: :desc).limit(2)
     @institutions = Employments::PublicCompetition.select(:institution_id, :institution_name, :institution_code).active.valid.uniq(:institution_id)
