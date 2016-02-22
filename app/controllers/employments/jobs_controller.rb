6bb86b20afb2efbd799d656495db0c3fb551fe15
Henry Díaz <henryalberto.diaz@gmail.com>
2016-02-22 13:58:56 -0600

    Cambios en empleos

--- app/controllers/employments/jobs_controller.rb
+++ app/controllers/employments/jobs_controller.rb
@@ -1,38 +1,31 @@
 class Employments::JobsController < EmploymentsController
   respond_to :html, :js
+  before_action :define_params
 
   def index
-    params[:q] ||= {}
     params[:q][:plaza_state_id_eq] = 2 unless params[:q][:plaza_state_id_eq].present?
     @search = Employments::PublicCompetition
       .active
       .order(created_date: :desc)
-    @search = @search.available if params[:q][:plaza_state_id_eq] == 2
+    @search = @search.available if params[:q][:plaza_state_id_eq].to_i == 2
     @jobs = @search
       .ransack(params[:q])
       .result(distinct: true)
       .paginate(page: params[:page], per_page: 5)
-    if params[:q][:institution_id_eq].present?
-      @institutions = Employments::PublicCompetition
-        .active
-        .select(:institution_id, :institution_name, :institution_code)
-        .where(institution_id: params[:q][:institution_id_eq])
-        .limit(1)
-    else
-      @institutions = Employments::PublicCompetition
-        .select(:institution_id, :institution_name, :institution_code)
-        .active
-        .uniq(:institution_id)
-        .ransack(params[:q])
-        .result(distinct: true)
-    end
+    @search = Employments::PublicCompetition
+      .select(:institution_id, :institution_name, :institution_code)
+      .active
+      .uniq(:institution_id)
+    @search = @search.available if params[:q][:plaza_state_id_eq].to_i == 2
+    @institutions = @search
+      .ransack(plaza_state_id_eq: params[:q][:plaza_state_id_eq])
+      .result(distinct: true)
 
     add_breadcrumb 'Inicio', employments_root_url
     add_breadcrumb "Plazas y empleos", employments_jobs_url
   end
 
   def show
-    params[:q] ||= {}
     @job = Employments::PublicCompetition.active.find(params[:id])
     redirect_to employments_jobs_url and return unless @job
     set_meta_tags og: {
@@ -54,5 +47,10 @@ class Employments::JobsController < EmploymentsController
     add_breadcrumb "Empleos disponibles", employments_jobs_url
     add_breadcrumb @job.post_name
   end
+
+  private
+    def define_params
+      params[:q] ||= {}
+    end
 
 end
