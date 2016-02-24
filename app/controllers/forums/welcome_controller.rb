2d56cfc11c048cdf3252f9c15efd014c3ef2baad
Henry Díaz <henryalberto.diaz@gmail.com>
2016-02-24 09:21:56 -0600

    Cambio en los filtros

--- app/controllers/forums/welcome_controller.rb
+++ app/controllers/forums/welcome_controller.rb
@@ -4,23 +4,12 @@ class Forums::WelcomeController < ForumsController
     @medias = Forum::Organization.where(kind: 'media').order(:name)
     @politics = Forum::Organization.where(kind: 'politic').order(:name)
     if @main
-      search = {}
-      if params[:q]
-        organization_ids = [params[:q][:media_id], params[:q][:political_id]]
-        organization_ids.reject!{|a| a==""}
-        unless organization_ids.blank?
-          search[:organization_id_in] = organization_ids
-        end
-        if params[:q][:year_eq].present?
-          search[:entry_at_gteq] = Date.new(params[:q][:year_eq].to_i, 1, 1)
-          search[:entry_at_lteq] = Date.new(params[:q][:year_eq].to_i, 12, 31)
-        end
-        case params[:q][:kind_eq].to_s
-          when 'Twitter'
-            search[:kind_eq] = 'twitter'
-        end
+      params[:q] ||= {}
+      if params[:q][:year_eq].present?
+        params[:q].merge!(entry_at_gteq: Date.new(params[:q][:year_eq].to_i, 1, 1))
+        params[:q].merge!(entry_at_lteq: Date.new(params[:q][:year_eq].to_i, 12, 31))
       end
-      @entries = @main.entries.ransack(search).result()
+      @entries = @main.entries.ransack(params[:q]).result()
       @years = @main.entries.pluck(:entry_at).map{|x| x.year}.uniq
     end
   end
