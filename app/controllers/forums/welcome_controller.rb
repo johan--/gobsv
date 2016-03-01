abbd3103a5d2ffec85ec8ee8aa7c6899570e30d6
Henry Díaz <henryalberto.diaz@gmail.com>
2016-03-01 08:23:38 -0600

    Paginación con parametros de busqueda

--- app/controllers/forums/welcome_controller.rb
+++ app/controllers/forums/welcome_controller.rb
@@ -1,17 +1,16 @@
 class Forums::WelcomeController < ForumsController
   def index
     @main = Forum::Theme.active.main.first
+    params[:q] ||= {}
+    params[:q].reject!{|_, v| v.blank?}
+    if params[:q][:year_eq].present?
+      params[:q].merge!(entry_at_gteq: Date.new(params[:q][:year_eq].to_i, 1, 1))
+      params[:q].merge!(entry_at_lteq: Date.new(params[:q][:year_eq].to_i, 12, 31))
+    end
     if request.xhr? || request.format.js?
       @entries = @main.entries.ransack(params[:q]).result(distinct: true).paginate(page: params[:page], per_page: 5)
     else
-      @medias = Forum::Organization.where(kind: 'media').order(:name)
-      @politics = Forum::Organization.where(kind: 'politic').order(:name)
       if @main
-        params[:q] ||= {}
-        if params[:q][:year_eq].present?
-          params[:q].merge!(entry_at_gteq: Date.new(params[:q][:year_eq].to_i, 1, 1))
-          params[:q].merge!(entry_at_lteq: Date.new(params[:q][:year_eq].to_i, 12, 31))
-        end
         @entries = @main.entries.ransack(params[:q]).result(distinct: true).paginate(page: params[:page], per_page: 5)
         @years = @main.entries.pluck(:entry_at).map{|x| x.year}.uniq
       end
