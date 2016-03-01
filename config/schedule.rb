113f41dea8b880ae1040502ed76427f62874e00e
Danilo Alfaro <alexander.danilo.alfaro@gmail.com>
2016-03-01 14:59:36 -0600

    Bot time
    Bot time

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,21 +24,21 @@ every :day, :at => '5:00am' do
   rake "employments:import"
 end
 
-#every 5.minutes do
-#  rake "forums:articles" 
-#end
+every 30.minutes do
+  rake "forums:articles" 
+end
 
 #every 120.minutes do
 #  rake "forums:retweets" 
 #end
 
-#every 60.minutes do
-#  rake "forums:documents" 
-#end
+every 60.minutes do
+  rake "forums:documents" 
+end
 
-#every 300.minutes do
-#  rake "forums:videos" 
-#end
+every 300.minutes do
+  rake "forums:videos" 
+end
 
 #every 300.minutes do
 #  rake "forums:facebook" 
