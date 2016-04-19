15b0dc9f0fb8cc3fd36d32b2fc3c22bd55dee7dc
Henry Díaz <henryalberto.diaz@gmail.com>
2016-04-18 22:00:10 -0600

    Cambio de estados

--- app/controllers/employments/jobs_controller.rb
+++ app/controllers/employments/jobs_controller.rb
@@ -1,13 +1,16 @@
 class Employments::JobsController < EmploymentsController
   respond_to :html, :js
+  #before_action :authenticate_user!, only: [:apply]
   before_action :define_params
+  before_action :get_job, except: [:index]
+  before_action :prepare_breadcrumb
 
   def index
-    params[:q][:plaza_state_id_eq] = 2 unless params[:q][:plaza_state_id_eq].present?
+    params[:q][:plaza_state_id_in] = ['2'] unless params[:q][:plaza_state_id_in].present?
     @search = Employments::PublicCompetition
       .active
       .order(created_date: :desc)
-    @search = @search.available if params[:q][:plaza_state_id_eq].to_i == 2
+    @search = @search.available if params[:q][:plaza_state_id_in].include?('2')
     @jobs = @search
       .ransack(params[:q])
       .result(distinct: true)
@@ -16,41 +19,32 @@ class Employments::JobsController < EmploymentsController
       .select(:institution_id, :institution_name, :institution_code)
       .active
       .uniq(:institution_id)
-    @search = @search.available if params[:q][:plaza_state_id_eq].to_i == 2
+    @search = @search.available if params[:q][:plaza_state_id_in].include?('2')
     @institutions = @search
-      .ransack(plaza_state_id_eq: params[:q][:plaza_state_id_eq])
+      .ransack(plaza_state_id_in: params[:q][:plaza_state_id_in])
       .result(distinct: true)
-
-    add_breadcrumb 'Inicio', employments_root_url
-    add_breadcrumb "Plazas y empleos", employments_jobs_url
   end
 
   def show
-    @job = Employments::PublicCompetition.active.find(params[:id])
-    redirect_to employments_jobs_url and return unless @job
     set_meta_tags og: {
                       title: @job.post_name,
                       description: @job.institution_name,
                       url: employments_job_url(@job),
                       image: 'http://www.empleospublicos.gob.sv' + view_context.image_path('employments/cv-girl.png')
                     }
-    add_breadcrumb 'Inicio', employments_root_url
-    add_breadcrumb "Empleos disponibles", employments_jobs_url
-    add_breadcrumb @job.post_name
-  end
-
-
-  def contact
-    @job = Employments::PublicCompetition.active.find(params[:id])
-    redirect_to employments_jobs_url and return unless @job
-    add_breadcrumb 'Inicio', employments_root_url
-    add_breadcrumb "Empleos disponibles", employments_jobs_url
-    add_breadcrumb @job.post_name
   end
 
   private
     def define_params
       params[:q] ||= {}
     end
-
+    def get_job
+      @job = Employments::PublicCompetition.active.find(params[:id])
+      redirect_to employments_jobs_url and return unless @job
+    end
+    def prepare_breadcrumb
+      add_breadcrumb 'Inicio', employments_root_url
+      add_breadcrumb 'Empleos disponibles', employments_jobs_url
+      add_breadcrumb @job.post_name if @job
+    end
 end
