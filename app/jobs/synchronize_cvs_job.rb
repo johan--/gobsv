f296fd638020383f78ac732c3ab6ec766c221f88
Henry Díaz <henryalberto.diaz@gmail.com>
2016-04-22 08:51:29 -0600

    Cambios empleos

--- app/jobs/synchronize_cvs_job.rb
+++ app/jobs/synchronize_cvs_job.rb
@@ -2,6 +2,18 @@ class SynchronizeCvsJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    # First get a valid token
+    uri = URI('http://www.funcionpublica.gob.sv/STPPplazas/token')
+    params = {
+      'Username' => Settings.sapt.username,
+      'Password' => Settings.sapt.password,
+      'Grant_Type' => Settings.sapt.grant_type
+    }
+    response = RestClient.post 'http://www.funcionpublica.gob.sv/STPPplazas/api/prueba',
+    {
+      url: 'http://empleospublicos.gob.sv/system/employments/user_specialties/certificates/000/000/001/original/guia-4.pdf',
+      nombre: "#{Time.current.to_i}_guia4.pdf"
+    },
+    {:Authorization => access_token}
   end
 end
