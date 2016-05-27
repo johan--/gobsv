5b4038ba2f45e1f40a6fc17492e88a6801abd3ee
Oscar <oscar@gobiernoabierto.gob.sv>
2016-05-27 14:45:21 -0600

    recuperar contraseña

--- app/models/user.rb
+++ app/models/user.rb
@@ -171,4 +171,33 @@ class User < ActiveRecord::Base
     SynchronizeUsersJob.perform_later self
   end
 
+
+  def send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    recoverable.send_reset_password_instructions if recoverable.persisted?
+    recoverable
+  end
+
+  # Attempt to find a user by its reset_password_token to reset its
+  # password. If a user is found and token is still valid, reset its password and automatically
+  # try saving the record. If not user is found, returns a new user
+  # containing an error in reset_password_token attribute.
+  # Attributes must contain reset_password_token, password and confirmation
+  def reset_password_by_token(attributes={})
+    original_token       = attributes[:reset_password_token]
+    reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
+
+    recoverable = find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
+
+    if recoverable.persisted?
+      if recoverable.reset_password_period_valid?
+        recoverable.reset_password(attributes[:password], attributes[:password_confirmation])
+      else
+        recoverable.errors.add(:reset_password_token, :expired)
+      end
+    end
+
+    recoverable.reset_password_token = original_token if recoverable.reset_password_token.present?
+    recoverable
+  end
 end
