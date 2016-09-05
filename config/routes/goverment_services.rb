5d420bcd9b83c767adf2aa0c7385c76c6cff9d5d
Oscar <oscar@gobiernoabierto.gob.sv>
2016-09-05 14:05:01 -0600

    subdominio servicios

--- config/routes/goverment_services.rb
+++ config/routes/goverment_services.rb
@@ -1,12 +1,12 @@
-domain = 'servicios.gobiernoabierto.gob.sv' if Rails.env.production?
 domain = 'localhost.com'          if Rails.env.development?
 
-constraints DomainConstraint.new(domain) do
+constraints subdomain: 'servicios' do
   scope module: 'goverment_services', as: 'goverment_services' do
+    defaults subdomain: 'servicios' do
 
-    root to: 'welcome#index'
+        root to: 'welcome#index'
 
-    resources :institution_services, only: [:index, :show]
-
+        resources :institution_services, only: [:index, :show]      
+    end
   end
 end
